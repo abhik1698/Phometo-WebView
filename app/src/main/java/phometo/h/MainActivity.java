ac4112ce87e9abf3cc2d54dc70bae85358b0c7a6
Abhishek K <abhik1698@gmail.com>
2018-10-07 21:23:08 +0530

    Finalized WebView Commit

--- app/src/main/java/phometo/h/MainActivity.java
+++ app/src/main/java/phometo/h/MainActivity.java
@@ -1,30 +1,33 @@
 package phometo.h;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
 
-    @SuppressLint("SetJavaScriptEnabled")
+    @SuppressLint({"SetJavaScriptEnabled", "ResourceAsColor"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         webView = findViewById(R.id.web);
         final ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.VISIBLE);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient() {
-
             @Override
             public void onPageStarted(WebView view, String url, Bitmap facIcon) {
                 progressBar.setVisibility(View.VISIBLE);//showProgressBar();
@@ -38,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 //show webview
                 findViewById(R.id.web).setVisibility(View.VISIBLE);
             }
-
         });
         webView.loadUrl("https://phometo.com/");
         final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeToRefresh);
@@ -55,8 +57,22 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         if (webView.canGoBack())
             webView.goBack();
-        else
-            super.onBackPressed();
-    }
+        else {
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.btn_radio)
+                    .setTitle("Exit")
+                    .setMessage("Are you sure?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(MainActivity.this, getString(R.string.seeyou), Toast.LENGTH_SHORT).show();
+                            MainActivity.super.onDestroy();
+                            finish();
+                        }
 
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+        }
+    }
 }
